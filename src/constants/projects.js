2a274876814ec0fb1f7b77ceb1ad5ca320b93c98
Snehit Paunikar <snehit@Snehits-MacBook-Air.local>
2022-09-03 22:56:14 +0530

    Added calculator and tictactoe projects

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -14,4 +14,14 @@ export const Projects=[
     link:"https://newsfeed-snehit.netlify.app/",
     description:"Newsfeed provides top 10 news for politics, sports and space categories"
     },
+    {
+    title:"TicTacToe",
+    link:"https://redarcher9.github.io/TicTacToe/",
+    description:"TicTacToe webpage built using ReactJS,HTML and CSS."
+    },
+    {
+    title:"React Calculator",
+    link:"https://redarcher9.github.io/React-Calculator/",
+    description:"Calculator app built using ReactJS, CSS and HTML."
+    },
 ];
